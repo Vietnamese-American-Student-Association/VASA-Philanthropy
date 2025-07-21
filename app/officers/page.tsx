1932e3a827ccb5b493d5a23a6a200516d84ee328
Jefferson Li <lijefferson027@gmail.com>
2025-07-20 21:07:02 -0400

    mobile optimization for officer page

--- app/officers/page.tsx
+++ app/officers/page.tsx
@@ -5,27 +5,36 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import BoardToggle from "./components/BoardToggle";
 import OfficersBoard from "./components/OfficersBoard";
+import MobileOfficersBoard from "./components/MobileOfficersBoard";
+import MobileHeader from "../components/MobileHeader";
+import MobileFooter from "../components/MobileFooter";
+import useIsMobile from "../hooks/useIsMobile";
 
 const OfficersPage: React.FC = () => {
   const [activeBoard, setActiveBoard] = useState<"executive" | "general">("executive");
+  const isMobile = useIsMobile();
 
   return (
     <div id="page-container">
-      <Header />
+      {isMobile ? <MobileHeader /> : <Header />}
       <main>
         <div className="content-wrapper">
           <div className="board-header">
-            <h2>
+            <h2 className="text-2xl text-green-500">
               {activeBoard === "executive" ? "Executive Board Officers" : "General Board Officers"}
             </h2>
             <div className="board-toggle-container">
               <BoardToggle activeBoard={activeBoard} setActiveBoard={setActiveBoard} />
             </div>
           </div>
-          <OfficersBoard type={activeBoard === "executive" ? "eboard" : "gboard"} />
+          {isMobile ? (
+            <MobileOfficersBoard type={activeBoard === "executive" ? "eboard" : "gboard"} />
+          ) : (
+            <OfficersBoard type={activeBoard === "executive" ? "eboard" : "gboard"} />
+          )}
         </div>
       </main>
-      <Footer />
+      {isMobile ? <MobileFooter /> : <Footer />}
     </div>
   );
 };
