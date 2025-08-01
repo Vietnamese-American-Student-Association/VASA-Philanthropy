c79b3c919e3cbb048bda25c9ae3378d85995851b
Jefferson Li <lijefferson027@gmail.com>
2025-07-31 23:47:46 -0400

    finished adding all the officer photos

--- app/officers/components/OfficersBoard.tsx
+++ app/officers/components/OfficersBoard.tsx
@@ -6,23 +6,23 @@ const executiveBoard: Officer[] = [
   { name: 'Huy Phan', position: 'External Vice President', image: '/images/huy.png' },
   { name: 'Jinju Nam', position: 'Internal Vice President', image: '/images/jinju.png' },
   { name: 'Dorothy Ta', position: 'Secretary', image: '/images/dorothy.png' },
-  { name: 'Jason Lai', position: 'Events Coordinator', image: '/images/jinju.png' },
+  { name: 'Jason Lai', position: 'Events Coordinator', image: '/images/jason.png' },
   { name: 'Stella Nguyen', position: 'Treasurer', image: '/images/stella.png' },
 ];
 
 const generalBoard: Officer[] = [
   { name: 'Jeff Li', position: 'Philanthropy Chair', image: '/images/jeff.png' },
-  { name: 'Alex Tran', position: 'Skit Chair', image: '/images/dort.png' },
-  { name: 'Natalie Escobar', position: 'Skit Chair', image: '/images/jinju.png' },
-  { name: 'Soleil Pham', position: 'Decorations Chair', image: '/images/huy.png' },
-  { name: 'Caden Bianes', position: 'Decorations Chair', image: '/images/madeline.png' },
-  { name: 'Julian Libutan', position: 'Public Relations Chair', image: '/images/jaz.png' },
-  { name: 'Vinh Nguyen-Luu', position: 'Public Relations Chair', image: '/images/dort.png' },
-  { name: 'Aileena Nguyen', position: 'Cultural Chair', image: '/images/jinju.png' },
-  { name: 'Anna Nguyen', position: 'Cultural Chair', image: '/images/huy.png' },
-  { name: 'Tristan Nguyen', position: 'Media Chair', image: '/images/jaz.png' },
-  { name: 'My-Linh Eberhard', position: 'Media Chair', image: '/images/dort.png' },
-  { name: 'Devin Chin', position: 'Media Chair', image: '/images/jinju.png' },
+  { name: 'Alex Tran', position: 'Skit Chair', image: '/images/alex.png' },
+  { name: 'Natalie Escobar', position: 'Skit Chair', image: '/images/nat.png' },
+  { name: 'Soleil Pham', position: 'Decorations Chair', image: '/images/soleil.png' },
+  { name: 'Caden Bianes', position: 'Decorations Chair', image: '/images/caden.png' },
+  { name: 'Julian Libutan', position: 'Public Relations Chair', image: '/images/julian.png' },
+  { name: 'Vinh Nguyen-Luu', position: 'Public Relations Chair', image: '/images/vinh.jpg' },
+  { name: 'Aileena Nguyen', position: 'Cultural Chair', image: '/images/aileena.png' },
+  { name: 'Anna Nguyen', position: 'Cultural Chair', image: '/images/anna.jpg' },
+  { name: 'Tristan Nguyen', position: 'Media Chair', image: '/images/tristan.png' },
+  { name: 'My-Linh Eberhard', position: 'Media Chair', image: '/images/mylinh.png' },
+  { name: 'Devin Chin', position: 'Media Chair', image: '/images/devin.png' },
 ];
 
 type OfficersBoardProps = {
